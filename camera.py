ee4c98923fce2449838155bc0efacd8f0d0f75cf
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-04-02 00:16:05 +0100

    Add camera module

---
+++ camera.py
@@ -0,0 +1,13 @@
+import cv2
+
+def capture_image(camera_idx):
+    camera = cv2.VideoCapture(camera_idx)
+    try:
+        if not camera.isOpened():
+            raise Exception(f"Failed to open camera {camera_idx}.")
+        rtrn, frame = camera.read()
+        if not rtrn:
+            raise Exception("Failed to capture image.")
+    finally:
+        camera.release()  # Force flush the camera
+    return frame
