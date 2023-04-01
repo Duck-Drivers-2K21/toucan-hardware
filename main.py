c4afea49b392808642fa6394862ccf4503f447bc
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-04-02 00:41:24 +0100

    Add conditional imports and fix function definitions

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import rpi
+from time import sleep
+import cv2
+
+RPI = False  # Set to `True` if you're using our embedded set-up
+CAMERA_IDX = 0  # The camera number to use
+
+if RPI:
+  from rpi import get_frame
+else:
+  from camera import get_frame
 
 for i in range(5):
-  rpi.get_frame()
+  cv2.imwrite(f'out{i}.png', get_frame(CAMERA_IDX))
