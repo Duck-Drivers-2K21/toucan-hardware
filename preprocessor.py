0abe2838358f579d35e42acd23d37125f96cda22
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-04-01 19:35:48 +0100

    Add image cropping

--- preprocessor.py
+++ preprocessor.py
@@ -27,8 +27,26 @@ def pair_wise_match(img1, img2):
   res[0 : img2.shape[0], 0 : img2.shape[1]] = img2
   print(res.shape)
   cv2.imwrite("result.png", res)  # TODO: Add date & time in the name
-  # TODO: Cut down the final image to not include the empty portion.
+  cv2.imwrite("cropped_res.png", crop_image(res))
+  return res
+
+def crop_image(img):
+  # Go from the rightmost position and start moving left
+  columns = img.sum(axis=0)
+  co_point = 0  # Cutoff point
+  for col in range(columns.shape[0] - 1, -1, -1):
+    if np.all(columns[col] == 0):
+      co_point = col
+    else:
+      break
+  print(co_point)
+  return img[:, :co_point]
 
 def combine_images(images: list):
   # TODO: Extend to work with multiple images (we want to do it pairwise)
   pass
+
+
+image1 = cv2.imread('image1.png')
+image2 = cv2.imread('image2.png')
+pair_wise_match(image1, image2)
