7f6faad901748bf06863ff22d44d54b60b0c8564
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-04-01 17:27:47 +0100

    Add image capture

--- driver.py
+++ driver.py
@@ -1,5 +1,27 @@
+import cv2
 import RPi.GPIO as GPIO
 import time
+
+def set_pos(pwm, pos):
+    dc_range = (5, 10)
+    dc = dc_range[0] + (pos / 180) * (dc_range[1] - dc_range[0])  # Duty cycle
+    pwm.ChangeDutyCycle(dc)
+    time.sleep(0.1)
+    pwm.ChangeDutyCycle(0)
+    time.sleep(2)
+
+def capture_image(camera):
+    rtrn, frame = camera.read()
+    if not rtrn:
+        print("Failed to capture image!")
+    return frame
+
+
+camera = cv2.VideoCapture(0)  # We're using a single camera
+
+if not camera.isOpened():
+    print("Failed to open camera! Restart.")
+    exit()
 
 frequency = 50  # in hz
 servo_pin = 22
@@ -10,20 +32,16 @@ GPIO.setup(servo_pin, GPIO.OUT)
 pwm = GPIO.PWM(servo_pin, frequency)
 pwm.start(0)
 
-def set_pos(pwm, pos):
-    dc_range = (5, 10)
-    dc = dc_range[0] + (pos / 180) * (dc_range[1] - dc_range[0])  # Duty cycle
-    pwm.ChangeDutyCycle(dc)
-    time.sleep(0.1)
-    pwm.ChangeDutyCycle(0)
-    time.sleep(2)
-
 try:
     for pos in range(0, 181, 45):
         print(pos)
         # TODO: Take a picture here. Place it in a tmp directory then combine them together.
         set_pos(pwm, pos)
     set_pos(pwm, 0)
+    out = capture_image(camera)
+    cv2.waitKey(0)
 except KeyboardInterrupt:
+    camera.release()
+    cv2.destroyAllWindows()
     pwm.stop()
     GPIO.cleanup()
