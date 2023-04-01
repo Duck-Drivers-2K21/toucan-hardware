9238520bf1afc2e64901b2ad2943c24a208170bc
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-04-02 00:55:13 +0100

    Remove leftover observation.py ref

--- rpi.py
+++ rpi.py
@@ -46,7 +46,7 @@ def get_frame(camera_idx):
     pwm.start(0)
 
     print(f"Capturing images (reverse={reverse})")
-    images = observation.capture_view(camera_idx, pwm, reverse)
+    images = capture_view(camera_idx, pwm, reverse)
     # for i in range(len(images)):
     #     cv2.imwrite(f"img{i}.png", images[i])
     pwm.stop()
